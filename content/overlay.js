a80d9993100715e8b3ba37f82a7415e6d66136b3
ikstrm <mail@ikst.jp>
2013-04-05 01:17:39 +0900

    Change target URL

--- content/overlay.js
+++ content/overlay.js
@@ -1,28 +1,28 @@
 (function() {
   jQuery.noConflict();
   $ = function(selector,context) { 
-    return new jQuery.fn.init(selector,context||example.doc); 
+    return new jQuery.fn.init(selector, context || rps.doc); 
   };
   $.fn = $.prototype = jQuery.fn;
 
-  example = new function(){};
-  example.log = function() { 
+  rps = new function(){};
+  rps.log = function() { 
     Firebug.Console.logFormatted(arguments,null,"log"); 
   };
-  example.run = function(doc,aEvent) {
+  rps.run = function(doc,aEvent) {
     // Check for website
-    if (!doc.location.href.match(/^http:\/\/(.*\.)?stackoverflow\.com(\/.*)?$/i))  
+    if (!doc.location.href.match(/^http:\/\/p\.eagate\.573\.jp(\/.*)?$/i))  
       return;
 
     // Check if already loaded
-    if (doc.getElementById("plugin-example")) return;
+    if (doc.getElementById("plugin-rps")) return;
 
     // Setup
     this.win = aEvent.target.defaultView.wrappedJSObject;
     this.doc = doc;
 
     // Hello World
-    this.main = main = $('<div id="plugin-example">').appendTo(doc.body).html('Example Loaded!');
+    this.main = main = $('<div id="plugin-rps">').appendTo(doc.body).html('Rps Loaded!');
     main.css({ 
       background:'#FFF',color:'#000',position:'absolute',top:0,left:0,padding:8
     });
@@ -32,7 +32,7 @@
   // Bind Plugin
   var delay = function(aEvent) { 
     var doc = aEvent.originalTarget; setTimeout(function() { 
-      example.run(doc,aEvent); 
+      rps.run(doc,aEvent); 
     }, 1); 
   };
   var load = function() { 
