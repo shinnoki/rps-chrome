b7fbb4d322a76c602e3efa67b3e0d0b7d68401b9
ikstrm <mail@ikst.jp>
2013-04-05 13:52:17 +0900

    Add button

--- content/overlay.js
+++ content/overlay.js
@@ -6,27 +6,23 @@
   $.fn = $.prototype = jQuery.fn;
 
   rps = new function(){};
-  rps.log = function() { 
-    Firebug.Console.logFormatted(arguments,null,"log"); 
-  };
   rps.run = function(doc,aEvent) {
     // Check for website
     if (!doc.location.href.match(/^http:\/\/p\.eagate\.573\.jp(\/.*)?$/i))  
       return;
 
     // Check if already loaded
-    if (doc.getElementById("plugin-rps")) return;
+    if (doc.getElementById("btn-rps")) return;
 
     // Setup
-    this.win = aEvent.target.defaultView.wrappedJSObject;
     this.doc = doc;
 
-    // Hello World
-    this.main = main = $('<div id="plugin-rps">').appendTo(doc.body).html('Rps Loaded!');
-    main.css({ 
-      background:'#FFF',color:'#000',position:'absolute',top:0,left:0,padding:8
-    });
-    main.html(main.html() + ' - jQuery <b>' + $.fn.jquery + '</b>');
+    // Add button
+    $('<input id="btn-rps" type="button" value="RPSにスコアを送信">')
+      .prependTo(doc.body).html('Rps Loaded!');
+    $('#btn-rps').click(function() {
+      $('<p>読込中...</p>').prependTo(doc.body);
+    }); 
   };
 
   // Bind Plugin
@@ -39,5 +35,4 @@
     gBrowser.addEventListener("DOMContentLoaded", delay, true); 
   };
   window.addEventListener("pageshow", load, false);
-
 })();
