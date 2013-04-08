b9fa1d044642a5b4e5dd09a8599510a04fa8aa03
ikstrm <mail@ikst.jp>
2013-04-08 18:35:18 +0900

    Save temp source

--- content/load_score.js
+++ content/load_score.js
@@ -1,18 +1,14 @@
 function loadScoreData() {
-  // var list = 0;
-  // var sort = 2;
-  // var page = 1;
-  // var uri = 'http://p.eagate.573.jp/game/2dx/20/p/djdata/music.html';
+  var list = 0;
+  var sort = 2;
+  var page = 1;
+  var uri = 'http://p.eagate.573.jp/game/2dx/20/p/djdata/music.html';
 
-  // uri += '?list=' + 0;
-  // uri += '&s=' + sort;
-  // uri += '&page=' + page;
-  alert('test');
+  uri += '?list=' + 0;
+  uri += '&s=' + sort;
+  uri += '&page=' + page;
 
-  // var uri = 'index.html';
-  // $.get(uri, function(data) {
-  //   alert(typeof(data));
-  // });
-  // alert('test2');
-
+  $.get(uri, function(data) {
+    alert(data);
+  });
 }
