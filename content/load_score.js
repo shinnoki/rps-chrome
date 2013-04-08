f5b035a3c871312a004d65a364fc171be4ee82af
ikstrm <mail@ikst.jp>
2013-04-08 18:33:42 +0900

    Update version of jquery

--- content/load_score.js
+++ content/load_score.js
@@ -1,13 +1,18 @@
 function loadScoreData() {
-  var list = 0;
-  var sort = 2;
-  var page = 1;
-  var uri = 'http://p.eagate.573.jp/game/2dx/20/p/djdata/music.html';
+  // var list = 0;
+  // var sort = 2;
+  // var page = 1;
+  // var uri = 'http://p.eagate.573.jp/game/2dx/20/p/djdata/music.html';
 
-  uri += '?list=' + 0;
-  uri += '&s=' + sort;
-  uri += '&page=' + page;
+  // uri += '?list=' + 0;
+  // uri += '&s=' + sort;
+  // uri += '&page=' + page;
+  alert('test');
 
-  alert(uri);
+  // var uri = 'index.html';
+  // $.get(uri, function(data) {
+  //   alert(typeof(data));
+  // });
+  // alert('test2');
+
 }
-
