a033aba4164a6ff522cc072d5cb2e326142858eb
ikstrm <mail@ikst.jp>
2013-04-26 23:49:17 +0900

    Write GET request by XMLHttpRequest

--- content/load_score.js
+++ content/load_score.js
@@ -3,17 +3,30 @@ function loadScoreData() {
   var sort = 2;
   var page = 1;
   var url = 'http://p.eagate.573.jp/game/2dx/20/p/djdata/music.html';
+  var url2 = 'http://p.eagate.573.jp/game/2dx/20/p/djdata/music_info.html?index=1';
 
-  url += '?list=' + 0;
+  url += '?list=' + list;
   url += '&s=' + sort;
   url += '&page=' + page;
+  url += '&' + (new Date().getTime());
 
-  Firebug.Console.log('year');
+
   Firebug.Console.log(url);
-  var reqest = new XMLHttpRequest();
-  reqest.open('GET', url, true);
 
-  if (request.status == 200) {
-    Firebug.Console.log(request);
-  }
+  var xhr= new XMLHttpRequest();
+
+  xhr.open("GET", url, true);
+  xhr.send();
+
+  func = function() {
+    xhr.open("GET", url2, true);
+    xhr.send();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        Firebug.Console.log(xhr.responseText);
+      }
+    };
+  };
+  xhr.onreadystatechange = func;
+  getAndCallback(nil, nil);
 }
